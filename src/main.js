923a30e364dd5f4b40daffa115902a96500c886a
NandaGilang11 <nandagilang@students.amikom.ac.id>
2023-06-14 10:03:45 +0700

    first commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import "bootstrap/dist/css/bootstrap.css";
+import 'bootstrap'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import "./assets/css/main.css";
+
 
 createApp(App).mount('#app')
